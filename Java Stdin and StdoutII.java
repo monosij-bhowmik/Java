f1ec2db7909c4aa513782baf3596a61db1452fb8
mail2monosij@gmail.com <mail2monosij@gmail.com>
2022-01-23 13:13:09 +0530

    Day 2

---
+++ Java Stdin and StdoutII.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String str = sc.nextLine();
+        sc.close();
+        System.out.println("String: "+str);
+        System.out.println("Double: "+d);
+        System.out.println("Int: "+n);
+    }
+}
